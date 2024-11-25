daacb9c530a3b1c08042790e2dcff2b6ae4a0935
Aryan-Kumar-7 <rajaryankumar26april@gmail.com>
2024-11-25 11:51:33 +0530

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://rajaryankumar26april2002:9889788246raK@cluster0.h1foya2.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGO_DB_URI).then(()=>console.log("DB Connected"));
 }
